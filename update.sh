11d6673ed8a0b42345b1a6bbb764a34339638823
Innovations Anonymous <InnovAnon-Inc@protonmail.com>
2021-02-01 20:52:05 -0600

    update

--- update.sh
+++ update.sh
@@ -8,7 +8,9 @@ for k in $(seq 5) ; do
   for K in $(seq $((2 ** k))) ; do
     sleep 91
   done
-  xbps-install -Suy || continue
+  #xbps-install -Suy || continue
+  apt update
+  apt full-upgrade -y || continue
   FLAG=1
   break
 done
@@ -19,8 +21,9 @@ for k in $(seq 7) ; do
   for K in $(seq $((2 ** k))) ; do
     sleep 91
   done
-  xbps-install   -y gettext gettext-devel gettext-libs gperf pkg-config po4a texinfo zip \
-                    python3-pillow-simd gtk+-devel glu-devel glib-devel fluidsynth-devel || continue
+  #xbps-install   -y gettext gettext-devel gettext-libs gperf pkg-config po4a texinfo zip \
+  #                  python3-pillow-simd gtk+-devel glu-devel glib-devel fluidsynth-devel || continue
+  apt install -y gettext gettext-tools gperf pkg-config po4a texinfo zip python3-pillow gtk+-dev glu-dev glib-dev fluidsynth-dev || continue
   FLAG=1
   break
 done
